c4dbbe9cc0a565675ecf2ffd8aaf1bdbd1071a36
Plabon Datta <plabondatta26@github.com>
2021-06-14 14:51:28 +0600

    Added project

---
+++ TAC_Pulse/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TAC_Pulse project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TAC_Pulse.settings')
+
+application = get_wsgi_application()

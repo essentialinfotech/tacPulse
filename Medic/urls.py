57aca09f67bf13594aef887834c9ac844db590dd
zahidfahad <zh.fahad123@gmail.com>
2021-06-16 11:32:02 +0600

    rates.js

--- Medic/urls.py
+++ Medic/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('audit/form/', audit_form, name = 'audit_form'),
     path('assesment/form/', assetment_form, name = 'assetment_form'),
+    path('rate/feedback/', rating, name = 'rating'),
 ]
--- Medic/views.py
+++ Medic/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 def audit_form(request):
     return render(request,'medic/audit_form.html')
 
+
 def assetment_form(request):
     return render(request,'medic/assesment_form.html')
+
+
+def rating(request):
+    return render(request,'medic/rate.html')

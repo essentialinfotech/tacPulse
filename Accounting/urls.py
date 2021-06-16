19a2f25f053f20580bfc1bf053fb7ae52b04aea5
plabondatta26 <plabondatta26@gmail.com>
2021-06-16 18:27:17 +0600

    some forms added

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -3,4 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('add/member/', add_member, name='add_member'),
+    path('add/package/', add_package, name='add_package'),
+    path('packages/', packages, name='packages'),
+    path('get/membership/', getmembership, name='getmembership'),
+    path('members/', members, name='members'),
+    path('schedule/trip/', shcedule_trip, name='shcedule_trip'),
+    path('scheduled/trips/', trip_schedules, name='trip_schedules'),
+    path('add/paystub/', add_paystub, name='add_paystub'),
+    path('paystub/report', paystub_report, name='paystub_report'),
 ]
--- Accounting/views.py
+++ Accounting/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
+
+
 def add_member(request):
-    return render(request, 'base.html')
+    return redirect('register')
+
+
+def add_package(request):
+    if request.method == 'POST':
+        return redirect('packages')
+    return render(request, 'Accounting/add_package.html')
+
+
+def packages(request):
+    return render(request, 'Accounting/packages.html')
+
+
+def getmembership(request):
+    return render(request, 'Accounting/get_membership.html')
+
+
+def members(request):
+    return render(request, 'Accounting/members.html')
+
+
+def shcedule_trip(request):
+    return render(request, 'Accounting/shcedule_trip.html')
+
+
+def trip_schedules(request):
+    return render(request, 'Accounting/trip_schedules.html')
+
+
+def add_paystub(request):
+    return render(request, 'Accounting/add_paystub.html')
+
+
+def paystub_report(request):
+    return render(request, 'Accounting/paystub_report.html')

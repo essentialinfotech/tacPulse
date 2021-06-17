cfb504ceea5295e226b126fdfe53387bfc965920
Plabon Datta <plabondatta26@github.com>
2021-06-17 18:08:45 +0600

    dragging

--- Medic/urls.py
+++ Medic/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('inspection/form/', inspection_form, name = 'inspection_form'),
     path('case/note/form/', case_note_form, name = 'case_note_form'),
     path('stock/request/form/', stock_req_form, name = 'stock_req_form'),
-    path('property/form/', tools_form, name = 'tools_form'), 
+    path('property/form/', tools_form, name = 'tools_form'),
+    path('occurrence/form/', occurrence_form, name = 'occurrence_form'),
+    path('panic/request/', panic_system, name = 'panic_system'),
+    path('draggable/form/', dragable_form, name = 'dragable_form'),
 ]
--- Medic/views.py
+++ Medic/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def audit_form(request):
@@ -26,10 +28,28 @@ def tools_form(request):
     return render(request,'medic/tools_form.html')
 
 
+def occurrence_form(request):
+    return render(request,'medic/occurrence_form.html')
+
+
+def dragable_form(request):
+    return render(request,'medic/dragable_form.html')
+
+
 def rating(request):
     return render(request,'medic/rate.html')
 
 
 def dispatch_list(request):
     return render(request,'medic/dispatch_list.html')
+
+
+def panic_system(request):
+    url = request.META.get('HTTP_REFERER')
+    panic = 'Give a panic request'
+    if request.method == 'POST':
+        messages.success(request,'Panic request sent')
+        return HttpResponseRedirect(url)
+    return render(request,'medic/panic.html',{'panic': panic})
+        
 

72b35193f3266430fa7c32cd3cf0403bc6d9b6b2
plabondatta26 <plabondatta26@gmail.com>
2021-06-17 16:53:42 +0600

    sidebar link added

--- Medic/views.py
+++ Medic/views.py
@@ -8,6 +8,10 @@ def audit_form(request):
     return render(request, 'medic/audit_form.html')
 
 
+def audit_report(request):
+    return render(request, 'medic/audit_report.html')
+
+
 def assetment_form(request):
     return render(request, 'medic/assesment_form.html')
 
@@ -16,12 +20,24 @@ def inspection_form(request):
     return render(request, 'medic/inspection_form.html')
 
 
+def inspaction_report(request):
+    return render(request, 'medic/inspaction_report.html')
+
+
 def case_note_form(request):
     return render(request, 'medic/case_note_form.html')
 
 
+def case_reports(request):
+    return render(request, 'medic/case_reports.html')
+
+
 def stock_req_form(request):
     return render(request, 'medic/stock_req_form.html')
+
+
+def stock_req_reports(request):
+    return render(request, 'medic/stock_req_reports.html')
 
 
 def tools_form(request):
@@ -34,8 +50,23 @@ def rating(request):
 
 def ambulance_request(request):
     return render(request, 'medic/ambulance_request.html')
-    return render(request, 'medic/rate.html')
+
+
+def ambulance_request_report(request):
+    return render(request, 'medic/ambulance_request_report.html')
 
 
 def dispatch_list(request):
     return render(request, 'medic/dispatch_list.html')
+
+
+def hospital_transfer(request):
+    return render(request, 'medic/hospita_transfer.html')
+
+
+def hospital_transfer_report(request):
+    return render(request, 'medic/hospita_transfer_report.html')
+
+
+def task_transfer_req(request):
+    return render(request, 'medic/task_transfer_req.html')

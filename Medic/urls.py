6de500745a5594b5739d9e5a169c51578a062fbb
plabondatta26 <plabondatta26@gmail.com>
2021-06-17 13:47:33 +0600

    base updated

--- Medic/urls.py
+++ Medic/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('audit/form/', audit_form, name = 'audit_form'),
-    path('assesment/form/', assetment_form, name = 'assetment_form'),
-    path('rate/feedback/', rating, name = 'rating'),
+    path('audit/form/', audit_form, name='audit_form'),
+    path('assesment/form/', assetment_form, name='assetment_form'),
+    path('rate/feedback/', rating, name='rating'),
+    path('ambulance/request/', ambulance_request, name='ambulance_request'),
 ]
--- Medic/views.py
+++ Medic/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+
+
 def audit_form(request):
-    return render(request,'medic/audit_form.html')
+    return render(request, 'medic/audit_form.html')
 
 
 def assetment_form(request):
-    return render(request,'medic/assesment_form.html')
+    return render(request, 'medic/assesment_form.html')
 
 
 def rating(request):
-    return render(request,'medic/rate.html')
+    return render(request, 'medic/rate.html')
+
+
+def ambulance_request(request):
+    return render(request, 'medic/ambulance_request.html')

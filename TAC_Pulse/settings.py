f4168b17ac5619ddbe90daecea9f0d1f55026f3e
zahidfahad <zh.fahad123@gmail.com>
2021-08-05 23:57:12 +0600

    payroll js

--- TAC_Pulse/settings.py
+++ TAC_Pulse/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'zh.fahad123@gmail.com'
-EMAIL_HOST_PASSWORD = 'zahidhassanfahad2851995django'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Mysql us/ps

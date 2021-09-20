0c096d7e5517c55584c7fd8b1cda62f0c279ae87
essentialinfotech <admin@essential-infotech.com>
2021-09-20 11:46:21 +0600

    pull

--- TAC_Pulse/settings.py
+++ TAC_Pulse/settings.py
@@ -156,7 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # EMAIL_USE_SSL = True
 # EMAIL_HOST = 'mail.essential-infotech.com'
 # EMAIL_HOST_USER = 'djangodeveloper@essential-infotech.com'
-# EMAIL_HOST_PASSWORD = 'Eit2020@@'
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_PORT = 465
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
